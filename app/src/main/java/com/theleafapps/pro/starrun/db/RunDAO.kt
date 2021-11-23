593ddde5a9d8813bf29218f69be4e77c5a5143c6
abhijeet <redefinedabhi2@gmail.com>
2021-11-24 03:11:09 +0530

    Created Run Entity & DAO

---
+++ app/src/main/java/com/theleafapps/pro/starrun/db/RunDAO.kt
@@ -0,0 +1,34 @@
+package com.theleafapps.pro.starrun.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RunDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeedInKMH(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistanceInMeters(): LiveData<List<Run>>
+
+}
