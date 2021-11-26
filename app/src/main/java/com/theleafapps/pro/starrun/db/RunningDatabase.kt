efdacee7bc9a16b67cb24c94e1b8f03496f286a4
abhijeet <redefinedabhi2@gmail.com>
2021-11-27 03:23:34 +0530

    Update RunningDatabase.kt
    Defined Running App Database

--- app/src/main/java/com/theleafapps/pro/starrun/db/RunningDatabase.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/db/RunningDatabase.kt
@@ -1,4 +1,17 @@
 package com.theleafapps.pro.starrun.db
 
-class RunningDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase{
+
+    abstract fun getRunDao(): RunDAO
+
 }
