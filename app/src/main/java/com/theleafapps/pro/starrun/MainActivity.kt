bd3bb5a9fc1920516680aca6458ec333e86d0053
abhijeet <redefinedabhi2@gmail.com>
2021-11-29 05:00:30 +0530

    AppModule Created
    To Provide Singleton Database Object

--- app/src/main/java/com/theleafapps/pro/starrun/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/MainActivity.kt
@@ -2,8 +2,14 @@ package com.theleafapps.pro.starrun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.theleafapps.pro.starrun.db.RunDAO
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/theleafapps/pro/starrun/other/Constants.kt
@@ -0,0 +1,7 @@
+package com.theleafapps.pro.starrun.other
+
+object Constants {
+
+    const val RUNNING_DATABASE_NAME = "running_db"
+
+}
