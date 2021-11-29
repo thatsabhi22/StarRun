78d5e3e7ed55a8d90a069ce7e6332e8af659d355
abhijeet <redefinedabhi2@gmail.com>
2021-11-30 05:21:26 +0530

    Update MainActivity.kt
    test

--- app/src/main/java/com/theleafapps/pro/starrun/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/MainActivity.kt
@@ -2,9 +2,12 @@ package com.theleafapps.pro.starrun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.theleafapps.pro.starrun.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     @Inject
@@ -13,5 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("runDao", "onCreate: runDao: ${runDAO.hashCode()}")
     }
 }
