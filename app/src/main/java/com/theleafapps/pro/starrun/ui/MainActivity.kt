05caf5e464437dac1c6aa026b6a807dfe4463372
abhijeet <redefinedabhi2@gmail.com>
2021-12-08 05:32:48 +0530

    Update MainActivity.kt
    Adding more imports

--- app/src/main/java/com/theleafapps/pro/starrun/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/ui/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.theleafapps.pro.starrun.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
