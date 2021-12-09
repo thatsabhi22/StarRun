fbff02716ff9fc6cbfe48ed70d2e203642dab01f
abhijeet <redefinedabhi2@gmail.com>
2021-12-10 04:18:49 +0530

    Created TrackingService
    Communicating between TrackingFragment & TrackingService

--- app/src/main/java/com/theleafapps/pro/starrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.theleafapps.pro.starrun.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.theleafapps.pro.starrun.R
+import com.theleafapps.pro.starrun.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.theleafapps.pro.starrun.services.TrackingService
 import com.theleafapps.pro.starrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
@@ -21,10 +24,19 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(),TrackingService::class.java).also{
+            it.action = action
+        }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
