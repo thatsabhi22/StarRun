3c649d124fc21b24fc8fd183156216b39ca3fa73
abhijeet <redefinedabhi2@gmail.com>
2022-01-31 06:28:18 +0530

    Setting up Shared Preferences for saving name and weight

--- app/src/main/java/com/theleafapps/pro/starrun/di/AppModule.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.theleafapps.pro.starrun.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.theleafapps.pro.starrun.db.RunningDatabase
+import com.theleafapps.pro.starrun.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.theleafapps.pro.starrun.other.Constants.KEY_NAME
+import com.theleafapps.pro.starrun.other.Constants.KEY_WEIGHT
 import com.theleafapps.pro.starrun.other.Constants.RUNNING_DATABASE_NAME
+import com.theleafapps.pro.starrun.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,5 +34,24 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context){
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+    }
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences)
+            = sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 
 }
--- app/src/main/java/com/theleafapps/pro/starrun/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/ui/fragments/SetupFragment.kt
@@ -1,19 +1,55 @@
 package com.theleafapps.pro.starrun.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.theleafapps.pro.starrun.R
+import com.theleafapps.pro.starrun.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.theleafapps.pro.starrun.other.Constants.KEY_NAME
+import com.theleafapps.pro.starrun.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         tvContinue.setOnClickListener{
-            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            val success = writePersonalDataToSharedPref()
+            if(success) {
+                findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            }
+            else{
+                Snackbar.make(requireView(),
+                    "Please enter all the fields", Snackbar.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun writePersonalDataToSharedPref(): Boolean {
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if(name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE,false)
+            .apply()
+
+        val toolbarText = "Let's go, $name!"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
     }
 }
