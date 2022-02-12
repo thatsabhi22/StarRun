c4f71a4da922e16b69e55e5bc9ffcab9e738f6ec
abhijeet <redefinedabhi2@gmail.com>
2022-02-12 09:58:12 +0530

    Bug Fix on Bottom Navigation Item Selected

--- app/src/main/java/com/theleafapps/pro/starrun/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/starrun/ui/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        bottomNavigationView.setOnItemReselectedListener {  }
 
         navHostFragment.findNavController()
             .addOnDestinationChangedListener{_,destination,_ ->
